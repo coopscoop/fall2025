57e28e476172837c7e60250f2cca7673bb46f56e
coop <coopscoop8515@gmail.com>
2025-09-15 01:40:36 -0400

    added a1 java template

--- math/approx_py.py
+++ math/approx_py.py
@@ -4,20 +4,20 @@ import math
 def calc_py(count):
     count_in_circle = 1
     total = 1
-    
-    
+
+
     for i in range(count):
         count = 1
         x = random.random()
         y = random.random()
-        
+
         euc_distance = math.sqrt((x - 0.5) ** 2 + (y - 0.5) ** 2)
         if euc_distance < 0.5:
             count_in_circle += 1
         total += 1
-        
+
         approx = (count_in_circle / total) * 4
-        
+
     print(approx)
-            
-calc_py(100000)
+
+calc_py(10000000)
